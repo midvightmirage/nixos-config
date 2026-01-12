611b38f41b364fe57c91057a4b339107c44905de
MidvightMirage <thepeanut003+@gmail.com>
2026-01-12 06:48:15 +0100

    1. Why did it prefill the commit message 2. Reformatted Config (Services)

--- nixosModules/services.nix
+++ nixosModules/services.nix
@@ -3,23 +3,22 @@
 {
   services =
   {
-  fprintd.enable = true;
-  fwupd.enable = true;
-  xserver.enable = true;
-  xserver.videoDrivers = [ "amdgpu" ];
-  displayManager.sddm.enable = true;
-  desktopManager.plasma6.enable = true;
-  xserver.xkb =
-  {
-    layout = "de";
-    variant = "";
-  };
-  libinput.enable = true;
-  services.printing.enable = true;
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire =
-  {
+    fprintd.enable = true;
+    fwupd.enable = true;
+    xserver.enable = true;
+    xserver.videoDrivers = [ "amdgpu" ];
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+    xserver.xkb =
+    {
+      layout = "de";
+      variant = "";
+    };
+    xserver.libinput.enable = true;
+    printing.enable = true;
+    pulseaudio.enable = false;
+    pipewire =
+    {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
@@ -28,8 +27,9 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
-
+    };
+    };
+    security.rtkit.enable = true;
   # SSH Daemon (uncomment if needed)
   # services.openssh.enable = true;
   # services.openssh.settings =
@@ -38,5 +38,4 @@
   #   passwordAuthentication = false;
   #   kbdInteractiveAuthentication = false;
   # };
-  };
 }
